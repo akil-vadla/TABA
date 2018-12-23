c822ec5dccddc369c2ce14b13522cb91d362a8db
akil-vadla <45454336+akil-vadla@users.noreply.github.com>
2018-12-23 22:38:02 +0530

    Add files via upload

---
+++ Dependencies.R
@@ -0,0 +1,8 @@
+if (!require(udpipe)){install.packages("shiny")}
+if (!require(udpipe)){install.packages("udpipe")}
+if (!require(udpipe)){install.packages("magrittr")}
+if (!require(udpipe)){install.packages("igraph")}
+if (!require(udpipe)){install.packages("ggraph")}
+if (!require(udpipe)){install.packages("ggplot2")}
+if (!require(udpipe)){install.packages("plyr")}
+if (!require(udpipe)){install.packages("wordcloud")}
